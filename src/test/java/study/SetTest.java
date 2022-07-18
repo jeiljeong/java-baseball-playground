842a0e35f23beaa03f2a8bec74fc3e7cb0e3f531
JeilJeong <wjdwpdlf12@gmail.com>
2022-07-19 02:44:26 +0900

    add new source file & TestCase Set.size()

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,33 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Set.size()")
+    void size() {
+        // given
+        int size = numbers.size();
+        // when
+        // then
+        assertThat(size).isEqualTo(3);
+    }
+}
